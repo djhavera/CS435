c15efc94c4f5c6c68f37564e42037a7423cde990
David Havera <djhavera@yahoo.com>
2025-02-27 12:41:54 -0600

    updates

--- linkstate.cpp
+++ linkstate.cpp
@@ -23,8 +23,60 @@ struct Node {
 void parseTopologyFile(const string& filename, map<int, Node>& nodes);
 void parseMessageFile(const string& filename, vector<string>& messages);
 void parseChangesFile(const string& filename, vector<tuple<int, int, int>>& changes);
-void writeForwardingTable(ofstream& outfile, const map<int, Node>& nodes);
-void writeMessages(ofstream& outfile, const vector<string>& messages, const map<int, Node>& nodes);
+void writeForwardingTable(ofstream& outfile, const map<int, Node>& nodes, map<int, map<int, int>>& routeNext, map<int, map<int, int>>& routeCost);
+void writeMessages(ofstream& outfile, const vector<string>& messages, const map<int, map<int, int>>& routeNext, const map<int, map<int, int>>& routeCost);
+
+void writeForwardingTable(ofstream& outfile, const map<int, Node>& nodes, map<int, map<int, int>>& routeNext, map<int, map<int, int>>& routeCost) {
+    for (const auto& [id, node] : nodes) {
+        outfile << "<forwarding table entries for node " << id << ">\n";
+        // Compute shortest paths using Dijkstra's algorithm
+        map<int, int> dist;
+        map<int, int> nextHop;
+        map<int, int> prevHop;
+        set<pair<int, int>> priority_queue;
+
+        for (const auto& [nid, _] : nodes) {
+            dist[nid] = numeric_limits<int>::max();
+        }
+        dist[id] = 0;
+        nextHop[id] = id; // The next hop for the node itself is the node itself
+        prevHop[id] = id;
+        priority_queue.insert({0, id});
+
+        while (!priority_queue.empty()) {
+            int u = priority_queue.begin()->second;
+            priority_queue.erase(priority_queue.begin());
+
+            for (const auto& [v, edge] : nodes.at(u).edges) {
+                int alt = dist[u] + edge.cost;
+                if (alt < dist[v] || (alt == dist[v] && u < prevHop[v])) {
+                    priority_queue.erase({dist[v], v});
+                    dist[v] = alt;
+                    nextHop[v] = (u == id) ? v : nextHop[u];
+                    prevHop[v] = u;
+                    priority_queue.insert({dist[v], v});
+                }
+            }
+        }
+
+        // Store the computed routes
+        routeNext[id] = nextHop;
+        routeCost[id] = dist;
+
+        // Write forwarding table for node id
+        vector<pair<int, pair<int, int>>> tableEntries;
+        for (const auto& [dest, cost] : dist) {
+            if (cost < numeric_limits<int>::max()) {
+                tableEntries.push_back({dest, {nextHop[dest], cost}});
+            }
+        }
+        sort(tableEntries.begin(), tableEntries.end());
+
+        for (const auto& entry : tableEntries) {
+            outfile << entry.first << " " << entry.second.first << " " << entry.second.second << "\n";
+        }
+    }
+}
 
 void parseTopologyFile(const string& filename, map<int, Node>& nodes) {
     ifstream infile(filename);
@@ -64,64 +116,7 @@ void parseChangesFile(const string& filename, vector<tuple<int, int, int>>& chan
     }
 }
 
-void writeForwardingTable(ofstream& outfile, const map<int, Node>& nodes) {
-    vector<int> nodeIds;
-    for (const auto& [id, _] : nodes) {
-        nodeIds.push_back(id);
-    }
-    sort(nodeIds.begin(), nodeIds.end());
-
-    for (int id : nodeIds) {
-        // outfile << "<forwarding table entries for node " << id << ">\n";
-        // Compute shortest paths using Dijkstra's algorithm
-        map<int, int> dist;
-        map<int, int> nextHop;
-        map<int, int> prevHop;
-        set<pair<int, int>> priority_queue;
-
-        for (const auto& [nid, _] : nodes) {
-            dist[nid] = numeric_limits<int>::max();
-        }
-        dist[id] = 0;
-        nextHop[id] = id; // The next hop for the node itself is the node itself
-        prevHop[id] = id;
-        priority_queue.insert({0, id});
-
-        while (!priority_queue.empty()) {
-            int u = priority_queue.begin()->second;
-            priority_queue.erase(priority_queue.begin());
-
-            for (const auto& [v, edge] : nodes.at(u).edges) {
-                int alt = dist[u] + edge.cost;
-                if (alt < dist[v] || (alt == dist[v] && u < prevHop[v])) {
-                    priority_queue.erase({dist[v], v});
-                    dist[v] = alt;
-                    nextHop[v] = (u == id) ? v : nextHop[u];
-                    prevHop[v] = u;
-                    priority_queue.insert({dist[v], v});
-                }
-            }
-        }
-
-        // Write forwarding table for node id
-        vector<pair<int, pair<int, int>>> tableEntries;
-        for (const auto& [dest, cost] : dist) {
-            if (cost < numeric_limits<int>::max()) {
-                tableEntries.push_back({dest, {nextHop[dest], cost}});
-            }
-        }
-        sort(tableEntries.begin(), tableEntries.end());
-
-        for (const auto& entry : tableEntries) {
-            if (entry.second.second < numeric_limits<int>::max()) {
-                outfile << entry.first << " " << entry.second.first << " " << entry.second.second << "\n";
-            }
-        }
-        outfile << "\n"; // Add blank line between tables
-    }
-}
-
-void writeMessages(ofstream& outfile, const vector<string>& messages, const map<int, Node>& nodes) {
+void writeMessages(ofstream& outfile, const vector<string>& messages, const map<int, map<int, int>>& routeNext, const map<int, map<int, int>>& routeCost) {
     // outfile << "<message output lines>\n";
     for (const auto& message : messages) {
         istringstream iss(message);
@@ -138,17 +133,17 @@ void writeMessages(ofstream& outfile, const vector<string>& messages, const map<
 
         while (current != dst) {
             path.push_back(current);
-            if (nodes.at(current).edges.find(dst) == nodes.at(current).edges.end()) {
+            if (routeNext.at(current).find(dst) == routeNext.at(current).end()) {
                 pathExists = false;
                 break;
             }
-            current = nodes.at(current).edges.at(dst).to;
+            current = routeNext.at(current).at(dst);
         }
         if (pathExists) {
             path.push_back(dst);
 
             // Write message with path
-            outfile << "from " << src << " to " << dst << " cost " << nodes.at(src).edges.at(dst).cost << " hops ";
+            outfile << "from " << src << " to " << dst << " cost " << routeCost.at(src).at(dst) << " hops ";
             for (size_t i = 0; i < path.size(); ++i) {
                 outfile << path[i];
                 if (i < path.size() - 1) {
@@ -173,6 +168,8 @@ int main(int argc, char** argv) {
     map<int, Node> nodes;
     vector<string> messages;
     vector<tuple<int, int, int>> changes;
+    map<int, map<int, int>> routeNext;
+    map<int, map<int, int>> routeCost;
 
     parseTopologyFile(argv[1], nodes);
     parseMessageFile(argv[2], messages);
@@ -181,8 +178,8 @@ int main(int argc, char** argv) {
     ofstream outfile("output.txt");
 
     // Initial state
-    writeForwardingTable(outfile, nodes);
-    writeMessages(outfile, messages, nodes);
+    writeForwardingTable(outfile, nodes, routeNext, routeCost);
+    writeMessages(outfile, messages, routeNext, routeCost);
 
     // Apply changes and write state after each change
     for (const auto& [from, to, cost] : changes) {
@@ -193,9 +190,9 @@ int main(int argc, char** argv) {
             nodes[from].edges[to] = {to, cost};
             nodes[to].edges[from] = {from, cost};
         }
-       // outfile << "----- At this point, change is applied\n";
-        writeForwardingTable(outfile, nodes);
-        writeMessages(outfile, messages, nodes);
+        outfile << "----- At this point, change is applied\n";
+        writeForwardingTable(outfile, nodes, routeNext, routeCost);
+        writeMessages(outfile, messages, routeNext, routeCost);
     }
 
     outfile.close();
